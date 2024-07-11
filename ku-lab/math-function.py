2359658f78811a8875c0795cd9afae54303d7cb2
1lDX <worrapon453@gmail.com>
2024-07-11 23:22:05 +0700

    solve python solution

--- ku-lab/math-function.py
+++ ku-lab/math-function.py
@@ -5,12 +5,12 @@ y = float(input("Enter y: "))
 z = float(input("Enter z: "))
 
 a1 = f"{math.pow(x,y) + z:.2f}"
-a2 = f"{math.cos(math.pi*2) + math.log(x):.2f}"#
+a2 = f"{math.cos(math.pi*2) + math.log(x):.2f}"
 a3 = f"{abs(x) + abs(y):.2f}"
 a4 = f"{math.sqrt(math.pow(x,2)+math.pow(y,2)+math.pow(z,2)):.2f}"
 a5 = f"{math.pow(math.sin(x),2) + math.pow(math.cos(x),2):.2f}"
 a6 = f"{round((x+y)**(1/5), 6):.2f}"
-a7 = f"{math.e**(x*math.log(y)):.2f}"#
+a7 = f"{math.e**(x*math.log(y)):.2f}"
 print(f"a1 = {a1}")
 print(f"a2 = {a2}")
 print(f"a3 = {a3}")
---
+++ programming-th-python/8.X2.py
@@ -0,0 +1,3 @@
+x1,S = input().split()
+x2 = int(S)*2-int(x1)
+print(x2)
